31e1e6c59764bf821328008bbeb34d5abedce3e9
Andrey Mishchenko <lbisbiy_andrey@mail.ru>
2016-08-02 13:09:39 +0300

    Game- created

---
+++ src/XO/model/Game.java
@@ -0,0 +1,29 @@
+package XO.model;
+
+
+public class Game {
+
+    private final Player[] players;
+
+    private final String name;
+
+    private final Field field;
+
+    public Game(final Player[] players, final String name, final Field field) {
+        this.players = players;
+        this.name = name;
+        this.field = field;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Field getField() {
+        return field;
+    }
+}
