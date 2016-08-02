fef240a0ad484912ac763f0e3857377e977eedd6
Mischenko.Andrey <lbisbiy_andrey@mail.ru>
2016-08-02 13:31:45 +0300

    GameTest - created

---
+++ test/XO/model/GameTest.java
@@ -0,0 +1,24 @@
+package XO.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class GameTest {
+    @Test
+    public void getPlayers() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+    @Test
+    public void getField() throws Exception {
+
+    }
+
+}
